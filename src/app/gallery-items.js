2501148df862a606f732f76823a5c4d72c748ba0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-19 21:14:54 +0200

    Create gallery-items.js

---
+++ src/app/gallery-items.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      'https://pixabay.com/get/g5937ab40bc507a27c6fefee70ba4ad7da2707f8cf7696350135d6530acdacb4a994958e3d22f307b26f963f1ec2f11d7_640.jpg',
+    original:
+      'https://pixabay.com/get/gdaa67b5f256bab190e1276e43121360eccee32003a8c64a18dc05b041c49d8e4bcf0cffb1b099cb488668ae6f36909df6622272423666bf1a2a4a3f4740c8a93_1920.jpg',
+    description: 'Moss Spore Capsules',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g14fe86aaa31fdc1450b2329479cd455517149b456adc71df7a42734709a5863ba4df58b482e90936fb0919328ccfaaf1_640.jpg',
+    original:
+      'https://pixabay.com/get/g3e74e580cd7a91706e4f7153407b6e75d0012aa85c49f1f92087236ac546eba75b282a8002f42d4a81236496f6c9d8b148574a267a1d7ea381ee0c643da4d1e1_1920.jpg',
+    description: 'Green Hills',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gce047e52d283915ed6c615d2ae40554a6dfa9b71766ded2c8a05b4607737b98efb42118a88a4b086acf798396b8b3801_640.jpg',
+    original:
+      'https://pixabay.com/get/g6cb0c581f763d953cb38914873640e11b7391e0c244cede046cfd84ca0dee0d45177b4d2e95ebea147b396c7ec214ab83a9590a3de3fe93c2d3d92fa38b5b587_1920.jpg',
+    description: 'Alberta Canada Lake',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gbb161e7a5e56c5c379d00f7344be08af4eb4b5056cb68cf801eda965add49e6ac052e360e1133a275ce318291b7d749f_640.jpg',
+    original:
+      'https://pixabay.com/get/g227d713b312731e3cadd87e4bfe6c486ace158c7a28d41e294c58cf41f1681068b797bc5f28f5cbb110bae1cfb8286fec19a62e9d7c9d5193be8f47947dd630b_1920.jpgtps://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Japanese Quince Early Bloomer',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/ga85aa3f411d2027bfedc2a9cf83fa35de68731e02268f0530e342b4eaa3fa55864642f077d4bddb99315574c881c77ac_640.jpg',
+    original:
+      'https://pixabay.com/get/ge3a505647aced2e1f5f729eb9cc421b767c64505f801b1ab8f1c21fd1361e8fea35fb94586fea437b215951a3f3de743_1280.jpg',
+    description: 'Autumn River',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/ge87b7a4082ce2563b307425fa29130292618c5785fecda0745e06b4bea9144c40abefc3d977eaf1dc5007dd6e6dc07f2_640.jpg',
+    original:
+      'https://pixabay.com/get/gd9ca1f2b8d5464bb37ff05ed01582c7b1ae968c226654f3f7e0c9b1df081332d751e798dd62b56f261342da57dca993a3c825177bc06cc6c3b5a6ba63dbff867_1280.jpg',
+    description: 'Flowers Plants',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gbf6ff34d127b477e6bff470f755318bc8a84bae3883fa8007ba28dd12e64c530510c131beaa80045893885396f6dc69c_640.jpg',
+    original:
+      'https://pixabay.com/get/g0081bb6582997b75fde585234689a51cd2ecf315ef82be4cbd6252eb0d0931263629487113e85e014c9e56080c109f3a3e250985766e390dbe850be5d5ac0acf_1920.jpg',
+    description: 'Forest Sunset',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
