e8d1bbc3abbb24a9bea70809cc699954df63bb90
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-20 14:43:03 +0200

    update url img

--- src/app/gallery-items.js
+++ src/app/gallery-items.js
@@ -1,51 +1,51 @@
 export const galleryItems = [
   {
     preview:
-      'https://pixabay.com/get/g11c6cf9a32f103b54a32fb7d98cf2335db5e435cbc83458160e78a467cd8a9625cf27892552ca70723e7283d3ddc935e4241b47762b5abefd6bc8099665216704659a7081030fca6c833924433df20eb_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/nature-4473078_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896099477',
     original:
-      'https://pixabay.com/get/g93b6839613949a05730f57566a0302ba088c93e544ce56852148a7494198d45b5d24131e094d2f63b2711c5b01081026f7b48af9786848d44cd3f465124309c7c56d1634cae2019c9012ec623695917e_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/nature-4473078_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896099477',
     description: 'Moss Spore Capsules',
   },
   {
     preview:
-      'https://pixabay.com/get/g2fa63a401371c020a020b9243c4cd391257ccc2ab96bc03e518cf6f442abaa5cbd099ad90917caabe2216efb4fcb3eea634b29494cddc16b57930a18d2843a1944ba19c2a319370b663a14f1ac5f096b_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/rainbow-5324147_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896198331',
     original:
-      'https://pixabay.com/get/g9283f8ee24b5727465782f4908a70eb9a948897939030892af011c85667ed4eb5099865984ede4473d56f704cd39137753da779197552ebceff1cdff8e929fb6b382e7806a21a84cfae4b351db0543ea_1280.jpg',
+      'https://ik.imagekit.io/irinavn2011/rainbow-5324147_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896198331',
     description: 'Green Hills',
   },
   {
     preview:
-      'https://pixabay.com/get/ga73269ea9fe9402b16aca18979eba494069aa13a7b8df8307e28f41c2460fcc5c0740dc602fb9e450f4c02be9d0d8bc3_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/lake-5961239_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896305630',
     original:
-      'https://pixabay.com/get/g4c19c58099a120d3b432feb0e3e161193d126368eb8cd311755e1d5599ce5dc3c26f97a18c9e36ad5ed0cd8e50666348d056eb8b17b9559058a03eabcdc3be24_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/lake-5961239_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896305630',
     description: 'Alberta Canada Lake',
   },
   {
     preview:
-      'https://pixabay.com/get/g418c603473c1575405886ba1b966073c606e0ab56dbe2ff4fc986ee914070aecb7381649f1e12d2e4e7ce1d3b9275f48_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/japanese-quince-7788371_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896369286',
     original:
-      'https://pixabay.com/get/g841ee95a9761a2861c4f3ece26e7e4a75fa08f3fb80aa8fbcb0a36cd60f8ac71a2bd2a2212f4f66206b7b254b66868ce267b6c25eb26bccf3c5c72cd62eb6226_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/japanese-quince-7788371_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896369286',
     description: 'Japanese Quince Early Bloomer',
   },
   {
     preview:
-      'https://pixabay.com/get/gfe326a7615bc26f5b570882f0366f688b8e314f8802de855a9ea483ef5ac41ddccb99b91a27ddad3aa4ebbee48a7308c_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/summer-landscape-1330260.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676895859440',
     original:
-      'https://pixabay.com/get/g834daa31d93e457affd4a884cf91edb2307a2f1a174085d847d31738a6c57cb944eab8d998413d1bcb4a7ddf161aa8ed.jpg',
-    description: 'Autumn River',
+      'https://ik.imagekit.io/irinavn2011/summer-landscape-1330260.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676895859440',
+    description: 'Summer Landscape',
   },
   {
     preview:
-      'https://pixabay.com/get/gfc03f60fbb1552c52f9de38a97f77f4979432d5aec1a42094daf598e2bfc794b5860b48fd7d2e43747e1f1f67edf655a_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/waterfall-6274258_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896451810',
     original:
-      'https://pixabay.com/get/gbbdd5943ba5d5852f6cd1e18151c267a5204af92f65e02309989e6597ed32ea2c13ea1073053a321bb7d97d87fb21ae15417ec675554c1993a7d59363b455b38_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/waterfall-6274258_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896451810',
     description: 'Rainforest',
   },
   {
     preview:
-      'https://pixabay.com/get/gb5e7222efad2912fc7810dae14bd62c0eb4a126196f236868b04751ee9dba26b9816f0668f04557d0810c19b75eb1091_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/sunset-675847_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896507055',
     original:
-      'https://pixabay.com/get/g6f70eb1335756fc83733fc6ba3873161e20d583ba2a8c804c5c94867784b1a9128589bece4c93f8f8669ec5dcb17a1b0_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/sunset-675847_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896507055',
     description: 'Sunset',
   },
   {
@@ -64,23 +64,23 @@ export const galleryItems = [
   },
   {
     preview:
-      'https://pixabay.com/get/gb0ea0cb569627592ba0e43c47c56a3d3e9ac00a16ee3aac5f1daaea29ea573009d4b85a5aec675194315da3a4e5b62fb_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/mountain-2143877_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896838692',
     original:
-      'https://pixabay.com/get/g188c6adb2349853bffe0339d4b1f4dc24ef9d5299e76a6d09bca97274df4a6fab3154cb25bb829825301197cda870d00dfc093b57a118e1ecc2be0972c802b7c_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/mountain-2143877_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896838692',
     description: 'The Mountain in the Desert',
   },
   {
     preview:
-      'https://pixabay.com/get/g309a8efd186d8ea015c291e467d4151b898016e6691f4e1ba5c45279253a38652a19b586541d9484f5bfde20ad34053b_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/hd-wallpaper-190056_1280.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896573160',
     original:
-      'https://pixabay.com/get/g95f6dc17c22bff656ed3da352bf6b2f1c031c92d01ff37dc4125d1923276beba45ef178ad56c96aadeedca47c1f6a36ea445ad92ebd837e7f1db28ad36889c5c_1920.jpg',
-    description: 'The Road Trip',
+      'https://ik.imagekit.io/irinavn2011/hd-wallpaper-190056_1280.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896573160',
+    description: 'The Mountain`s Dog',
   },
   {
     preview:
-      'https://pixabay.com/get/g4c8a7b0949073db42511a150a2670570f3bf025e8003c46b3054b4dcb35fc278b13990473fcf503eacbe79859fa6ed43_640.jpg',
+      'https://ik.imagekit.io/irinavn2011/panama-4953165_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896907634',
     original:
-      'https://pixabay.com/get/g68b73fe4fa850356f83d13604cafc7902ae6e5a2d5b4758845230952ce83d3cec5efda8296eaf634a209cc5b018cd4857f69a1b394c650f3332e78c383d54b71_1920.jpg',
+      'https://ik.imagekit.io/irinavn2011/panama-4953165_1920.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676896907634',
     description: 'San Blas Islands',
   },
 ];
