7c385acd092ebb94a372d8bcf4df8d2189f76ca2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-25 22:23:47 +0200

    add script for three buttons: start, pause, stop to keep track of the elapsed time after the stopwatch has stopped and resume the stopwatch from the time the start button is pressed again.

--- src/app/05-stopwatch.js
+++ src/app/05-stopwatch.js
@@ -1,8 +1,12 @@
 const millisecondsInSec = 1000;
 const stopBtn = document.querySelector('.js-stop');
+const stopButton = document.querySelector('.js-stop__stopwatch');
 const startBtn = document.querySelector('.js-start');
+const startButton = document.querySelector('.js-start__stopwatch');
+const pauseButton = document.querySelector('.js-pause__stopwatch');
 const stopAudio = document.querySelector('.js-audio-stop');
 const startAudio = document.querySelector('.js-audio-start');
+const pauseAudio = document.querySelector('.js-audio-pause');
 
 let intervalId = null;
 let elapsedMilliseconds = 0;
@@ -18,7 +22,6 @@ const pause = () => {
 };
 
 const start = () => {
-  //const initialDate = new Date();
   const initialDate = new Date(Date.now() - elapsedMilliseconds);
   intervalId = setInterval(() => {
     const delta = new Date() - initialDate;
@@ -62,4 +65,64 @@ startBtn.addEventListener('click', e => {
   } else {
     start();
   }
+});
+
+// ------- next variant with 3 buttons ------------------------
+
+const interrupt = () => {
+  pause();
+  elapsedMilliseconds = 0;
+  renderStopWatch('0 days 00:00:00:000');
+};
+
+const renderStopwatch = str =>
+  (document.querySelector('.stopwatch-next').innerText = str);
+
+const begin = () => {
+  const initialDate = new Date(Date.now() - elapsedMilliseconds);
+  intervalId = setInterval(() => {
+    const delta = new Date() - initialDate;
+
+    elapsedMilliseconds = delta;
+    const milliseconds = Math.floor(delta % millisecondsInSec);
+    const seconds = Math.floor((delta / millisecondsInSec) % secondsInMin);
+    const minutes = Math.floor(
+      (delta / (millisecondsInSec * secondsInMin)) % minutesInHour
+    );
+    const hours = Math.floor(
+      (delta / (millisecondsInSec * secondsInMin * minutesInHour)) % hoursInDay
+    );
+    const days = Math.floor(
+      delta / (millisecondsInSec * secondsInMin * minutesInHour * hoursInDay)
+    );
+
+    const UISeconds = String(seconds).padStart(2, '0');
+    const UIMinutes = String(minutes).padStart(2, '0');
+    const UIHours = String(hours).padStart(2, '0');
+
+    const stopwatcherFormat = `${days} days ${UIHours} : ${UIMinutes} : ${UISeconds} : ${milliseconds}`;
+
+    renderStopwatch(stopwatcherFormat);
+  }, 0);
+};
+
+startButton.addEventListener('click', e => {
+  e.currentTarget.classList.toggle('paused');
+  startAudio.paused ? startAudio.play() : startAudio.pause();
+  if (intervalId) {
+    pause();
+  } else {
+    begin();
+  }
+});
+
+pauseButton.addEventListener('click', e => {
+  pause();
+});
+
+stopButton.addEventListener('click', e => {
+  stopAudio.play();
+  pause();
+  elapsedMilliseconds = 0;
+  renderStopwatch('0 days 00 : 00 : 00 : 000');
 });
